52e54eaeda344f213750c0126a7f1646151cea6f
Tariq Axel <tariqaxel@gmail.com>
2021-11-02 20:14:16 +0100

    Adding test conditions & image urls

--- Algorithm.js
+++ Algorithm.js
@@ -11,7 +11,18 @@ const imgpoolurl = [
     "https://images.unsplash.com/photo-1488866022504-f2584929ca5f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG5pZ2h0JTIwc2t5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
     "https://images.unsplash.com/photo-1569141510930-5a09d247488f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHN0YXJyeSUyMHNreXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
     "https://images.unsplash.com/photo-1591708370135-f2fac858e97b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-    
+    "https://media.istockphoto.com/photos/aerial-view-dramatic-sunset-and-sunrise-sky-nature-background-with-picture-id1183953762?k=20&m=1183953762&s=612x612&w=0&h=EdMjj1ro4jVeIlr0Z0Gw-vLC_lAegmITSKt5duRg7HU=",
+    "https://images.unsplash.com/photo-1607969391576-48f9021ded30?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2t5JTIwbmlnaHR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1587105020968-1cca5fdcfcec?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2t5JTIwbmlnaHR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1532767153582-b1a0e5145009?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2t5JTIwbmlnaHR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1572191783453-62f99a6055ce?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c2t5JTIwbmlnaHR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1622396481328-9b1b78cdd9fd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2t5JTIwZGF5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1590198513019-7cc432df1e6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c2t5JTIwZGF5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1566918583512-0c3dfc0d0bae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2t5JTIwZGF5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1576067389322-41215c09eb80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHNreSUyMGRheXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1559334641-7448ffca69e0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHNreSUyMGRheXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1596188126441-41279e8f5e46?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNreSUyMGRheXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1600152997022-56a2b234bfc4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHNreSUyMGRheXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
 ];
 var imgpoolload=[];
 
@@ -34,9 +45,25 @@ const trainingdata = [
         output:[1]
     },
     {
+        input: {r: 0, g: 0.51, b:0.51, a:1 },
+        output:[1]
+    },
+    {
+        input: {r: 0, g: 0.49, b:0.49, a:1 },
+        output:[0]
+    },
+    {
+        input: {r: 0, g: 0.80, b:0.49, a:1 },
+        output:[0]
+    },
+    {
+        input: {r: 0, g: 0.49, b:0.80, a:1 },
+        output:[0]
+    },
+    {
         input: {r: 0, g: 0, b:0, a:1 },
         output:[0]
-    }
+    },
 ];
 network.train(trainingdata);
 
@@ -57,8 +84,9 @@ guessAndDisplayNightOrDayBasedOnPixels = (pixelsRGBAData, resultdisplay) =>{
     //console.log({r: testdata[0]/datalength, g: testdata[1]/datalength, b:testdata[2]/datalength, a:testdata[3]/datalength});
    
     result=network.run({r: testdata[0]/datalength, g: testdata[1]/datalength, b:testdata[2]/datalength, a:testdata[3]/datalength});
-    resultdisplay.innerHTML= result < .5 ? "Night "+result : "Day "+result;
-    //console.log(resultdisplay.id+' '+result);
+    let certainty = result[0].toFixed(2)*100;
+    resultdisplay.innerHTML= result < 0.5 ? "Night "+(100-certainty)+"%" : "Day "+certainty+"%";
+    
 }
 
 function guessNightOrDayForAnImageUrl (imageToGuess){
