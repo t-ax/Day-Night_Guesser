34f5d246d80ef63874b582442057f6b66097954c
Tariq Axel <tariqaxel@gmail.com>
2021-11-12 14:12:30 +0100

    result css done

--- App/Routes/DayNight.routes.js
+++ App/Routes/DayNight.routes.js
@@ -1,9 +1,9 @@
-const INITIMAGE = "https://images.pexels.com/photos/53594/blue-clouds-day-fluffy-53594.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+const INITIMAGE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBWbDMtDygvRxsKqktoVTVjyw-wFhKqdYcAA&usqp=CAU"
 
 module.exports = app => {
     const DayNightController = require("../Controllers/DayNight.controller.js");
     
-    app.get('/', (req, res)=>{res.render("home", {'ImageUrl' : INITIMAGE, 'result': "Day", 'percentage': "100%", 'error': null})});
+    app.get('/', (req, res)=>{res.render("home", {'ImageUrl' : INITIMAGE, 'result': "Night", 'percentage': "XX%", 'error': null})});
     app.get('/runUserImage', DayNightController.CheckUrlAndExecuteServiceForEJS);
     app.get('/api', DayNightController.CheckUrlAndExecuteServiceForAPI);
 };
